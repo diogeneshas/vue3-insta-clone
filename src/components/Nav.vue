b1d24f07c7305bb57e11ad68778b3201d75ea480
diogeneshas <diogenes.henrique.br@gmail.com>
2023-05-30 07:39:38 -0300

    Building the NavBar

---
+++ src/components/Nav.vue
@@ -0,0 +1,46 @@
+<template>
+  <ALayoutHeader>
+    <Container>
+      <div class="nav-container">
+        <div class="right-content">
+          <RouterLink to="/">Instragram</RouterLink>
+          <a-input-search placeholder="input search text" v-model="searchUsername" @search="onSearch" style="width: 200px"/>
+        </div>
+        <div class="left-content">
+          <a-button type="primary">Signup</a-button>
+          <a-button type="primary">Login</a-button>
+        </div>
+      </div>
+    </Container>
+  </ALayoutHeader>
+</template>
+
+<script setup>
+import { RouterLink } from "vue-router"
+import Container from "./Container.vue"
+import { ref } from "vue"
+
+const searchUsername = ref("")
+
+const onSearch = () => {}
+</script>
+
+<style scoped>
+.nav-container {
+    display: flex;
+    justify-content: space-between;
+}
+
+.right-content {
+    display: flex;
+    align-items: center;
+}
+
+.right-content a {
+    margin-right: 10px;
+}
+
+.left-content button{
+  margin-left: 10px;
+}
+</style>
