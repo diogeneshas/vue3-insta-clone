46bfef38839d086efabeabaada06d0cc6be5a48d
diogeneshas <diogenes.henrique.br@gmail.com>
2023-06-08 10:40:10 -0300

    Signing Up the User

--- src/stores/users.js
+++ src/stores/users.js
@@ -1,5 +1,6 @@
 import { ref } from 'vue'
 import { defineStore } from 'pinia'
+import { supabase } from "../supabase"
 
 export const useUserStore = defineStore('users', () => {
   const user = ref(null)
@@ -17,7 +18,7 @@ export const useUserStore = defineStore('users', () => {
 
   }
 
-  const handleSignup = (credentials) => {
+  const handleSignup = async (credentials) => {
     const { email, password, username } = credentials
 
     if(password.length < 6) {
@@ -32,7 +33,27 @@ export const useUserStore = defineStore('users', () => {
       return errorMessage.value = "Email is invalid"
     }
 
+    const {data: userWithUsername } = await supabase.from("users").select().eq('username', username).single()
+
+    if(userWithUsername) {
+      return errorMessage.value = "User already registered"
+    }
+
     errorMessage.value = ""
+
+    const { error } = await supabase.auth.signUp({
+      email,
+      password
+    })
+
+    if(error) {
+      return errorMessage.value = error.message
+    }
+
+    await supabase.from("users").insert({
+      username,
+      email
+    })
   }
 
   const handleLogout = () => {
